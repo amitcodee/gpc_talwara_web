2a6865ac91483f80fc16d3edd8aae091a7d2834e
Amit Sharma <pandit143amit@gmail.com>
2023-11-21 15:28:46 +0530

    hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeroSectionComponent } from "./hero-section/hero-section.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, HeroSectionComponent]
 })
 export class AppComponent {
-  title = 'college_website';
+  title = 'Gpc Talwara';
 }
