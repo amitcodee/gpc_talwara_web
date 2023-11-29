517b491129c68f65300067cc35c9377b3db5fd59
Amit Sharma <pandit143amit@gmail.com>
2023-11-29 15:13:38 +0530

    update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,5 +9,15 @@ import { CommonModule } from '@angular/common';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  navbarScrolled = false;
+
+  @HostListener('window:scroll') 
+  onWindowScroll() {
+    if (window.pageYOffset > 50) {
+      this.navbarScrolled = true;
+    } else {
+      this.navbarScrolled = false;
+    }
+  }
 
 }
