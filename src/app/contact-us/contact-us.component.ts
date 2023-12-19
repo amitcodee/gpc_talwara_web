9055bf173b75a38a8caff22e56b65be525b609f8
Amit Sharma <pandit143amit@gmail.com>
2023-12-19 18:43:49 +0530

    dark-mode update

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LanguageService } from '../services/language/language.service';
+import { TextSizeService } from '../services/text-size/text-size.service';
+import { ThemeService } from '../services/theme/theme.service';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './contact-us.component.html',
-  styleUrl: './contact-us.component.scss'
+  styleUrl: './contact-us.component.scss',
 })
 export class ContactUsComponent {
+  isDarkMode = false;
+  showFixedBottomDiv = false;
 
+  constructor(
+    private textSizeService: TextSizeService,
+    private languageService: LanguageService,
+    private themeService: ThemeService
+  ) {}
+
+  ngOnInit() {
+    this.themeService.isDarkMode$.subscribe((darkMode: boolean) => {
+      this.isDarkMode = darkMode;
+      // Update component styles or perform other actions based on darkMode
+    });
+  }
+
+  getTextSize(): number {
+    return this.textSizeService.getTextSize();
+  }
+
+  getCurrentLanguage() {
+    return this.languageService.currentLanguage;
+  }
 }
