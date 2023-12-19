6028ef30577d1677f18974b31bad3cfccfe6234d
Amit Sharma <pandit143amit@gmail.com>
2023-12-19 14:33:49 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { RouterOutlet,RouterLink } from '@angular/router';
 import { HeaderComponent } from "./header-footer/header/header.component";
 import AOS from 'aos';
 import { HeroSectionComponent } from "./home/hero-section/hero-section.component";
+import { FooterComponent } from "./header-footer/footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet,RouterLink,HeaderComponent, HeroSectionComponent, ]
+    imports: [CommonModule, RouterOutlet, RouterLink, HeaderComponent, HeroSectionComponent, FooterComponent]
 })
 export class AppComponent implements OnInit{
   ngOnInit() {
