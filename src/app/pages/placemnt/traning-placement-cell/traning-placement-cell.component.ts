f8fe0ccbcaa499f19ce3d0f9bc94c05997fe085c
Nikhil2247 <153814768+Nikhil2247@users.noreply.github.com>
2023-12-26 19:23:08 +0530

    training placement section darkmode and ts fixed

--- src/app/pages/placemnt/traning-placement-cell/traning-placement-cell.component.ts
+++ src/app/pages/placemnt/traning-placement-cell/traning-placement-cell.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LanguageService } from '../../../services/language/language.service';
+import { TextSizeService } from '../../../services/text-size/text-size.service';
+import { ThemeService } from '../../../services/theme/theme.service';
 
 @Component({
   selector: 'app-traning-placement-cell',
@@ -9,5 +12,28 @@ import { CommonModule } from '@angular/common';
   styleUrl: './traning-placement-cell.component.scss'
 })
 export class TraningPlacementCellComponent {
+  isDarkMode = false;
+   showFixedBottomDiv = false;
+  languageOptions: string[] = ['English', 'ਪੰਜਾਬੀ','हिंदी'];
 
+  constructor(
+    private textSizeService: TextSizeService,
+    private languageService: LanguageService,
+    private themeService: ThemeService
+  ) {}
+
+  ngOnInit() {
+    this.themeService.isDarkMode$.subscribe((darkMode: boolean) => {
+      this.isDarkMode = darkMode;
+      // Update component styles or perform other actions based on darkMode
+    });
+  }
+
+  getTextSize(): number {
+    return this.textSizeService.getTextSize();
+  }
+
+  getCurrentLanguage() {
+    return this.languageService.currentLanguage;
+  }
 }
