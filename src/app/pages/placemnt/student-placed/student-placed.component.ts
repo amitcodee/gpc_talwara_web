e609ef8fc7f0b984f304cd8787aa1cf672eb8cd8
Nikhil2247 <153814768+Nikhil2247@users.noreply.github.com>
2023-12-26 19:22:35 +0530

    student placed section ts and darkmode fixed

--- src/app/pages/placemnt/student-placed/student-placed.component.ts
+++ src/app/pages/placemnt/student-placed/student-placed.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LanguageService } from '../../../services/language/language.service';
+import { TextSizeService } from '../../../services/text-size/text-size.service';
+import { ThemeService } from '../../../services/theme/theme.service';
+
 
 @Component({
   selector: 'app-student-placed',
@@ -9,5 +13,28 @@ import { CommonModule } from '@angular/common';
   styleUrl: './student-placed.component.scss'
 })
 export class StudentPlacedComponent {
+  isDarkMode = false;
+  showFixedBottomDiv = false;
+ languageOptions: string[] = ['English', 'ਪੰਜਾਬੀ','हिंदी'];
 
+ constructor(
+   private textSizeService: TextSizeService,
+   private languageService: LanguageService,
+   private themeService: ThemeService
+ ) {}
+
+ ngOnInit() {
+   this.themeService.isDarkMode$.subscribe((darkMode: boolean) => {
+     this.isDarkMode = darkMode;
+     // Update component styles or perform other actions based on darkMode
+   });
+ }
+
+ getTextSize(): number {
+   return this.textSizeService.getTextSize();
+ }
+
+ getCurrentLanguage() {
+   return this.languageService.currentLanguage;
+ }
 }
