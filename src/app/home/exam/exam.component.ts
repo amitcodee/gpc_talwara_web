be1048f59d2aea985fd9b7a88ad4a584ed890328
Nikhil2247 <153814768+Nikhil2247@users.noreply.github.com>
2023-12-28 11:32:29 +0530

    examination component dark mode fix

--- src/app/home/exam/exam.component.ts
+++ src/app/home/exam/exam.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LanguageService } from '../../services/language/language.service';
+import { ThemeService } from '../../services/theme/theme.service';
+import { TextSizeService } from '../../services/text-size/text-size.service';
 
 @Component({
   selector: 'app-exam',
@@ -9,5 +12,28 @@ import { CommonModule } from '@angular/common';
   styleUrl: './exam.component.scss'
 })
 export class ExamComponent {
+  isDarkMode = false;
+  showFixedBottomDiv = false;
+ languageOptions: string[] = ['English', 'ਪੰਜਾਬੀ','हिंदी'];
 
+ constructor(
+   private textSizeService: TextSizeService,
+   private languageService: LanguageService,
+   private themeService: ThemeService
+ ) {}
+
+ ngOnInit() {
+   this.themeService.isDarkMode$.subscribe((darkMode: boolean) => {
+     this.isDarkMode = darkMode;
+     // Update component styles or perform other actions based on darkMode
+   });
+ }
+
+ getTextSize(): number {
+   return this.textSizeService.getTextSize();
+ }
+
+ getCurrentLanguage() {
+   return this.languageService.currentLanguage;
+ }
 }
