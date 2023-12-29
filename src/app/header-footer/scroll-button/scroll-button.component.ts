850a47238fdf9eb711662a4d731d0c9b59c28bf5
Amit Sharma <pandit143amit@gmail.com>
2023-12-29 20:30:09 +0530

    fix dark mode in scroll btn

--- src/app/header-footer/scroll-button/scroll-button.component.ts
+++ src/app/header-footer/scroll-button/scroll-button.component.ts
@@ -1,6 +1,9 @@
 import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ScrollService } from '../../services/scroll/scroll.service';
+import { LanguageService } from '../../services/language/language.service';
+import { TextSizeService } from '../../services/text-size/text-size.service';
+import { ThemeService } from '../../services/theme/theme.service';
 
 @Component({
   selector: 'app-scroll-button',
@@ -11,8 +14,15 @@ import { ScrollService } from '../../services/scroll/scroll.service';
 })
 export class ScrollButtonComponent {
   showButton = false;
+  isDarkMode = false;
+  languageOptions: string[] = ['English', 'ਪੰਜਾਬੀ', 'हिंदी'];
 
-  constructor(private scrollService: ScrollService) {}
+  constructor(
+    private scrollService: ScrollService,
+    private textSizeService: TextSizeService,
+    private languageService: LanguageService,
+    private themeService: ThemeService
+  ) {}
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
@@ -22,5 +32,30 @@ export class ScrollButtonComponent {
 
   scrollToTop(): void {
     window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  ngOnInit() {
+    // Subscribe to dark mode changes
+    this.themeService.isDarkMode$.subscribe((darkMode: boolean) => {
+      this.isDarkMode = darkMode;
+      // Update component styles or perform other actions based on darkMode
+    });
+  }
+
+  getTextSize(): number {
+    return this.textSizeService.getTextSize();
+  }
+
+  getCurrentLanguage() {
+    return this.languageService.currentLanguage;
+  }
+
+  // You can add methods for toggling dark mode and changing language here
+  toggleDarkMode() {
+    this.themeService.toggleDarkMode();
+  }
+
+  changeLanguage(language: string) {
+    this.languageService.changeLanguage(language);
   }
 }
