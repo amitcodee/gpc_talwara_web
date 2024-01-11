90629d418b11fbdbfbaef7d04bb321f9044064bf
Amit Sharma <pandit143amit@gmail.com>
2024-01-11 15:46:54 +0530

    update

--- src/app/home/news-section/news/news.component.ts
+++ src/app/home/news-section/news/news.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LanguageService } from '../../../services/language/language.service';
+import { TextSizeService } from '../../../services/text-size/text-size.service';
+import { ThemeService } from '../../../services/theme/theme.service';
 
 @Component({
   selector: 'app-news',
@@ -8,21 +11,54 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   templateUrl: './news.component.html',
 })
-export class NewsComponent {
+export class NewsComponent implements OnInit {
+  isDarkMode = false;
+  showFixedBottomDiv = false;
+  languageOptions: string[] = ['English', 'ਪੰਜਾਬੀ', 'हिंदी'];
+  selectedNews: any;
+  imagePath: any;
+
+  constructor(
+    private textSizeService: TextSizeService,
+    private languageService: LanguageService,
+    private themeService: ThemeService
+  ) {}
+
+  ngOnInit() {
+    this.themeService.isDarkMode$.subscribe((darkMode: boolean) => {
+      this.isDarkMode = darkMode;
+    });
+
+    this.selectedNews = this.newsList[0];
+  }
+
+  getTextSize(): number {
+    return this.textSizeService.getTextSize();
+  }
+
+  getCurrentLanguage() {
+    return this.languageService.currentLanguage;
+  }
+
+  selectNews(news: any): void {
+    this.selectedNews = news;
+  }
   newsList = [
     {
       "id": "first_news",
       "title": "Admission Process Started",
       "imagePath": "../../../assets/images/used/addmision/2.webp",
       "description": "Admission process for session 2023-24 started",
-      "link": "https://eakadamik.in/psbte_2022/"
+      "link": "https://eakadamik.in/psbte_2022/",
+      "punjabi_title": "ਦਾਖਲਾ ਹੈਲਪਲਾਈਨ ਡੈਸਕ",
     },
     {
       "id": "second_news",
       "title": "Admission Helpline Desk",
       "imagePath": "../../../assets/images/used/addmision/3.webp",
       "description": "Contact for admission process: 9501107354, 9501648500, 7009110372, 8437700636, 9417433471,7696889004",
-      "link": "/second-news"
+      "link": "/second-news",
+     
 
     },
     {
@@ -43,16 +79,4 @@ export class NewsComponent {
 
     }
   ];
-
-  selectedNews: any;
-imagePath: any;
-
-  ngOnInit(): void {
-    this.selectedNews = this.newsList[0];
-  }
-
-  selectNews(news: any): void {
-    this.selectedNews = news;
-  }
-  
 }
