6c94dc4d21fe44153a67f1808c8f4e4bf97d9b96
Amit Sharma <pandit143amit@gmail.com>
2024-02-02 22:36:53 +0530

    update

--- src/app/pages/about-us/mandotory-disclore/mandotory-disclore.component.ts
+++ src/app/pages/about-us/mandotory-disclore/mandotory-disclore.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { LanguageService } from '../../../services/language/language.service';
 import { TextSizeService } from '../../../services/text-size/text-size.service';
 import { ThemeService } from '../../../services/theme/theme.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-mandotory-disclore',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './mandotory-disclore.component.html',
   styleUrl: './mandotory-disclore.component.scss'
 })
@@ -32,8 +33,11 @@ export class MandotoryDiscloreComponent {
   getTextSize(): number {
     return this.textSizeService.getTextSize();
   }
-
+  
   getCurrentLanguage() {
     return this.languageService.currentLanguage;
+  }
+  scrollToTopAndCloseNavbar() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 }
